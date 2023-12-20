46ef0ec95397fd6e342efba5dfbc57ad9be91029
Nam Trinh Giang <namgt283@gmail.com>
2023-12-20 19:44:07 +0700

    2nd

--- client.c
+++ client.c
@@ -20,20 +20,20 @@ int user_function(int sock,int choice);
 int crud_flight(int sock,int choice);
 int crud_user(int sock,int choice);
 
-int main(void) { 
+int main(int argc, char const *argv[]) { 
 	int sock; 
     	struct sockaddr_in server; 
     	char server_reply[50],*server_ip;
-	server_ip = "127.0.0.1"; 
+		//server_ip = "127.0.0.1"; 
      
     	sock = socket(AF_INET, SOCK_STREAM, 0); 
     	if (sock == -1) { 
        	printf("Could not create socket"); 
     	} 
     
-    	server.sin_addr.s_addr = inet_addr(server_ip); 
+    	server.sin_addr.s_addr = inet_addr(argv[1]); 
     	server.sin_family = AF_INET; 
-    	server.sin_port = htons(PORT); 
+    	server.sin_port = htons(atoi(argv[2])); 
    
     	if (connect(sock, (struct sockaddr*)&server, sizeof(server)) < 0)
        	perror("connect failed. Error"); 
@@ -83,7 +83,7 @@ int client(int sock){
 		char name[50],password[50],secret_pin[6];
 		system("clear");
 		printf("\n\tEnter The Type Of Account:: \n");
-		printf("\t0. Admin\n\t1. Agent\n\t2. Customer\n");
+		printf("\t0. Admin\n\t2. Customer\n");
 		printf("\tYour Response: ");
 		scanf("%d", &type);
 		printf("\tEnter Your Name: ");
@@ -163,281 +163,18 @@ int menu(int sock,int type){
 }
 
 //-------------------------------- crud operations on flight-----------------------------//
-int crud_flight(int sock,int choice){
-	int valid = 0;
-	if(choice==1){				// Add flight response
-		char tname[50];
-		printf("\n\tEnter flight name: ");
-		scanf("%s",tname);
-		write(sock, &tname, sizeof(tname));
-		read(sock,&valid,sizeof(valid));	
-		if(valid)
-			printf("\n\tFlight added successfully\n");
+ int crud_flight(int sock,int choice){
+ 	return 0;
+ }
 
-		return valid;	
-	}
-	
-	else if(choice==2){			// View flight response
-		int no_of_flights;
-		int tno;
-		char tname[50];
-		int tseats;
-		int aseats;
-		read(sock,&no_of_flights,sizeof(no_of_flights));
+// //-------------------------------- crud operations on user-----------------------------//
 
-		printf("\tT_no\tT_name\tT_seats\tA_seats\n");
-		while(no_of_flights--){
-			read(sock,&tno,sizeof(tno));
-			read(sock,&tname,sizeof(tname));
-			read(sock,&tseats,sizeof(tseats));
-			read(sock,&aseats,sizeof(aseats));
-			
-			if(strcmp(tname, "deleted")!=0)
-				printf("\t%d\t%s\t%d\t%d\n",tno,tname,tseats,aseats);
-		}
+ int crud_user(int sock,int choice){
+	return 0;
+ }
 
-		return valid;	
-	}
-	
-	else if (choice==3){			// Update flight response
-		int tseats,choice=2,valid=0,tid;
-		char tname[50];
-		write(sock,&choice,sizeof(int));
-		crud_flight(sock,choice);
-		printf("\n\t Enter the flight number you want to modify: ");
-		scanf("%d",&tid);
-		write(sock,&tid,sizeof(tid));
-		
-		printf("\n\t1. Flight Name\n\t2. Total Seats\n");
-		printf("\t Your Choice: ");
-		scanf("%d",&choice);
-		write(sock,&choice,sizeof(choice));
-		
-		if(choice==1){
-			read(sock,&tname,sizeof(tname));
-			printf("\n\t Current name: %s",tname);
-			printf("\n\t Updated name:");
-			scanf("%s",tname);
-			write(sock,&tname,sizeof(tname));
-		}
-		else if(choice==2){
-			read(sock,&tseats,sizeof(tseats));
-			printf("\n\t Current value: %d",tseats);
-			printf("\n\t Updated value:");
-			scanf("%d",&tseats);
-			write(sock,&tseats,sizeof(tseats));
-		}
-		read(sock,&valid,sizeof(valid));
-		if(valid)
-			printf("\n\t Flight data updated successfully\n");
-		return valid;
-	}
-
-	else if(choice==4){				// Delete flight response
-		int choice=2,tid,valid=0;
-		write(sock,&choice,sizeof(int));
-		crud_flight(sock,choice);
-		
-		printf("\n\t Enter the flight number you want to delete: ");
-		scanf("%d",&tid);
-		write(sock,&tid,sizeof(tid));
-		read(sock,&valid,sizeof(valid));
-		if(valid)
-			printf("\n\t Flight deleted successfully\n");
-		return valid;
-	}
-	
-}
-
-//-------------------------------- crud operations on user-----------------------------//
-
-int crud_user(int sock,int choice){
-	int valid = 0;
-	if(choice==1){							// Add user
-		int type,id;
-		char name[50],password[50];
-		printf("\n\tEnter The Type Of Account:: \n");
-		printf("\t1. Agent\n\t2. Customer\n");
-		printf("\tYour Response: ");
-		scanf("%d", &type);
-		printf("\tUser Name: ");
-		scanf("%s", name);
-		strcpy(password,getpass("\tPassword: "));
-		write(sock, &type, sizeof(type));
-		write(sock, &name, sizeof(name));
-		write(sock, &password, strlen(password));
-		read(sock,&valid,sizeof(valid));	
-		if(valid){
-			read(sock,&id,sizeof(id));
-			printf("\tRemember Your login id For Further Logins as :: %d\n", id);
-		}
-		return valid;	
-	}
-	
-	else if(choice==2){						// View user list
-		int no_of_users;
-		int id,type;
-		char uname[50];
-		read(sock,&no_of_users,sizeof(no_of_users));
-
-		printf("\tU_id\tU_name\tU_type\n");
-		while(no_of_users--){
-			read(sock,&id,sizeof(id));
-			read(sock,&uname,sizeof(uname));
-			read(sock,&type,sizeof(type));
-			
-			if(strcmp(uname, "deleted")!=0)
-				printf("\t%d\t%s\t%d\n",id,uname,type);
-		}
-
-		return valid;	
-	}
-
-	else if (choice==3){						// Update user
-		int choice=2,valid=0,uid;
-		char name[50],pass[50];
-		write(sock,&choice,sizeof(int));
-		crud_user(sock,choice);
-		printf("\n\t Enter the U_id you want to modify: ");
-		scanf("%d",&uid);
-		write(sock,&uid,sizeof(uid));
-		
-		printf("\n\t1. User Name\n\t2. Password\n");
-		printf("\t Your Choice: ");
-		scanf("%d",&choice);
-		write(sock,&choice,sizeof(choice));
-		
-		if(choice==1){
-			read(sock,&name,sizeof(name));
-			printf("\n\t Current name: %s",name);
-			printf("\n\t Updated name:");
-			scanf("%s",name);
-			write(sock,&name,sizeof(name));
-			read(sock,&valid,sizeof(valid));
-		}
-		else if(choice==2){
-			printf("\n\t Enter Current password: ");
-			scanf("%s",pass);
-			write(sock,&pass,sizeof(pass));
-			read(sock,&valid,sizeof(valid));
-			if(valid){
-				printf("\n\t Enter new password:");
-				scanf("%s",pass);
-			}
-			else
-				printf("\n\tIncorrect password\n");
-			
-			write(sock,&pass,sizeof(pass));
-		}
-		if(valid){
-			read(sock,&valid,sizeof(valid));
-			if(valid)
-				printf("\n\t User data updated successfully\n");
-		}
-		return valid;
-	}
-
-	else if(choice==4){						// Delete a user
-		int choice=2,uid,valid=0;
-		write(sock,&choice,sizeof(int));
-		crud_user(sock,choice);
-		
-		printf("\n\t Enter the id you want to delete: ");
-		scanf("%d",&uid);
-		write(sock,&uid,sizeof(uid));
-		read(sock,&valid,sizeof(valid));
-		if(valid)
-			printf("\n\t User deleted successfully\n");
-		return valid;
-	}
-}
-
-//-------------------------------- User function to book tickets -----------------------------//
-int user_function(int sock,int choice){
-	int valid =0;
-	if(choice==1){										// Book tickets
-		int view=2,tid,seats;
-		write(sock,&view,sizeof(int));
-		crud_flight(sock,view);
-		printf("\n\tEnter the flight number you want to book: ");
-		scanf("%d",&tid);
-		write(sock,&tid,sizeof(tid));
-				
-		printf("\n\tEnter the no. of seats you want to book: ");
-		scanf("%d",&seats);
-		write(sock,&seats,sizeof(seats));
-	
-		read(sock,&valid,sizeof(valid));
-		if(valid)
-			printf("\n\tTicket booked successfully.\n");
-		else
-			printf("\n\tSeats were not available.\n");
-
-		return valid;
-	}
-	
-	else if(choice==2){									// View the bookings
-		int no_of_bookings;
-		int id,tid,seats;
-		read(sock,&no_of_bookings,sizeof(no_of_bookings));
-
-		printf("\tB_id\tT_no\tSeats\n");
-		while(no_of_bookings--){
-			read(sock,&id,sizeof(id));
-			read(sock,&tid,sizeof(tid));
-			read(sock,&seats,sizeof(seats));
-			
-			if(seats!=0)
-				printf("\t%d\t%d\t%d\n",id,tid,seats);
-		}
-
-		return valid;
-	}
-
-	else if(choice==3){									// Update a booking (increment/ decrement seats)
-		int choice = 2,bid,val,valid;
-		user_function(sock,choice);
-		printf("\n\t Enter the B_id you want to modify: ");
-		scanf("%d",&bid);
-		write(sock,&bid,sizeof(bid));
-
-		printf("\n\t1. Increase number of seats\n\t2. Decrease number of seats\n");
-		printf("\t Your Choice: ");
-		scanf("%d",&choice);
-		write(sock,&choice,sizeof(choice));
-
-		if(choice==1){
-			printf("\n\tNo. of tickets to increase");
-			scanf("%d",&val);
-			write(sock,&val,sizeof(val));
-		}
-		else if(choice==2){
-			printf("\n\tNo. of tickets to decrease");
-			scanf("%d",&val);
-			write(sock,&val,sizeof(val));
-		}
-		read(sock,&valid,sizeof(valid));
-		if(valid)
-			printf("\n\tBooking updated successfully.\n");
-		else
-			printf("\n\tUpdation failed. No more seats available.\n");
-		return valid;
-	}
-	
-	else if(choice==4){									// Cancel the entire booking
-		int choice = 2,bid,valid;
-		user_function(sock,choice);
-		printf("\n\t Enter the B_id you want to cancel: ");
-		scanf("%d",&bid);
-		write(sock,&bid,sizeof(bid));
-		read(sock,&valid,sizeof(valid));
-		if(valid)
-			printf("\n\tBooking cancelled successfully.\n");
-		else
-			printf("\n\tCancellation failed.\n");
-		return valid;
-	}
-	else if(choice==5)									// Logout
-		return -1;
-	
-}
+// //-------------------------------- User function to book tickets -----------------------------//
+ int user_function(int sock,int choice){
+	if (choice == 5) return -1;
+	else return 0;
+ }
--- server.c
+++ server.c
@@ -13,15 +13,28 @@ socket->bind->listen->accept->recv/send->close
 #include <fcntl.h>
 #include <netinet/in.h>
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define MAX_BUFFER_SIZE 256
 
 #define PORT 8000
 
 //--------------- structures declaration of flight and user------------//
 struct flight{
-		int flight_number;
+		int flight_id;
 		char flight_name[50];
-		int total_seats;
-		int available_seats;
+        char depart_point[50];
+        char destination_point[50];
+        char depart_date[50];
+        char return_date[50];
+		int total_ecoSeats;
+		int available_ecoSeats;
+        int total_busiSeats;
+		int available_busiSeats;
 		};
 struct user{
 		int login_id;
@@ -34,7 +47,7 @@ struct booking{
 		int booking_id;
 		int type;
 		int uid;
-		int tid;
+		int fid;
 		int seats;
 		};
 //---------------------------------------------------------------------//
@@ -43,9 +56,7 @@ void service_cli(int sock);
 void login(int client_sock);
 void signup(int client_sock);
 int menu(int client_sock,int type,int id);
-void crud_flight(int client_sock);
-void crud_user(int client_sock);
-int user_function(int client_sock,int choice,int type,int id);
+
    
 
 int client_count = 0;  // Global variable to track connection count
@@ -118,15 +129,6 @@ void service_cli(int sock){
 
 //-------------------- Login function-----------------------------//
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <fcntl.h>
-#include <unistd.h>
-
-#define MAX_BUFFER_SIZE 256
-
-
 
 void login(int client_sock) {
     int fd_user = open("db/db_user.txt", O_RDWR);
@@ -175,7 +177,7 @@ void login(int client_sock) {
         close(fd_user);
     }
 
-    // if valid user, show him menu
+    // if valid user, show menu
     if (user_valid) {
         write(client_sock, &valid, sizeof(valid));
         if (valid) {
@@ -245,11 +247,9 @@ int menu(int client_sock,int type,int id){
 	if(type==0){
 		read(client_sock,&choice,sizeof(choice));
 		if(choice==1){					// CRUD options on flight
-			crud_flight(client_sock);
 			return menu(client_sock,type,id);	
 		}
 		else if(choice==2){				// CRUD options on User
-			crud_user(client_sock);
 			return menu(client_sock,type,id);
 		}
 		else if (choice ==3)				// Logout
@@ -257,550 +257,9 @@ int menu(int client_sock,int type,int id){
 	}
 	else if(type==2 || type==1){				// For agent and customer
 		read(client_sock,&choice,sizeof(choice));
-		ret = user_function(client_sock,choice,type,id);
 		if(ret!=5)
 			return menu(client_sock,type,id);
 		else if(ret==5)
 			return -1;
 	}		
-}
-
-//---------------------- CRUD operation on flight--------------------//
-
-void crud_flight(int client_sock){
-	int valid=0;	
-	int choice;
-	read(client_sock,&choice,sizeof(choice));
-	if(choice==1){  					// Add flight  	
-		char tname[50];
-		int tid = 0;
-		read(client_sock,&tname,sizeof(tname));
-		struct flight tdb,temp;
-		struct flock lock;
-		int fd_flight = open("db/db_flight.txt", O_RDWR);
-		
-		tdb.flight_number = tid;
-		strcpy(tdb.flight_name,tname);
-		tdb.total_seats = 10;				// by default, we are taking 10 seats
-		tdb.available_seats = 10;
-
-		int fp = lseek(fd_flight, 0, SEEK_END); 
-
-		lock.l_type = F_WRLCK;
-		lock.l_start = fp;
-		lock.l_len = 0;
-		lock.l_whence = SEEK_SET;
-		lock.l_pid = getpid();
-
-		fcntl(fd_flight, F_SETLKW, &lock);
-
-		if(fp == 0){
-			valid = 1;
-			write(fd_flight, &tdb, sizeof(tdb));
-			lock.l_type = F_UNLCK;
-			fcntl(fd_flight, F_SETLK, &lock);
-			close(fd_flight);
-			write(client_sock, &valid, sizeof(valid));
-		}
-		else{
-			valid = 1;
-			lseek(fd_flight, -1 * sizeof(struct flight), SEEK_END);
-			read(fd_flight, &temp, sizeof(temp));
-			tdb.flight_number = temp.flight_number + 1;
-			write(fd_flight, &tdb, sizeof(tdb));
-			write(client_sock, &valid,sizeof(valid));	
-		}
-		lock.l_type = F_UNLCK;
-		fcntl(fd_flight, F_SETLK, &lock);
-		close(fd_flight);
-		
-	}
-
-	else if(choice==2){					// View/ Read flights
-		struct flock lock;
-		struct flight tdb;
-		int fd_flight = open("db/db_flight.txt", O_RDONLY);
-		
-		lock.l_type = F_RDLCK;
-		lock.l_start = 0;
-		lock.l_len = 0;
-		lock.l_whence = SEEK_SET;
-		lock.l_pid = getpid();
-		
-		fcntl(fd_flight, F_SETLKW, &lock);
-		int fp = lseek(fd_flight, 0, SEEK_END);
-		int no_of_flights = fp / sizeof(struct flight);
-		write(client_sock, &no_of_flights, sizeof(int));
-
-		lseek(fd_flight,0,SEEK_SET);
-		while(fp != lseek(fd_flight,0,SEEK_CUR)){
-			read(fd_flight,&tdb,sizeof(tdb));
-			write(client_sock,&tdb.flight_number,sizeof(int));
-			write(client_sock,&tdb.flight_name,sizeof(tdb.flight_name));
-			write(client_sock,&tdb.total_seats,sizeof(int));
-			write(client_sock,&tdb.available_seats,sizeof(int));
-		}
-		valid = 1;
-		lock.l_type = F_UNLCK;
-		fcntl(fd_flight, F_SETLK, &lock);
-		close(fd_flight);
-	}
-
-	else if(choice==3){					// Update flight
-		crud_flight(client_sock);
-		int choice,valid=0,tid;
-		struct flock lock;
-		struct flight tdb;
-		int fd_flight = open("db/db_flight.txt", O_RDWR);
-
-		read(client_sock,&tid,sizeof(tid));
-
-		lock.l_type = F_WRLCK;
-		lock.l_start = (tid)*sizeof(struct flight);
-		lock.l_len = sizeof(struct flight);
-		lock.l_whence = SEEK_SET;
-		lock.l_pid = getpid();
-		
-		fcntl(fd_flight, F_SETLKW, &lock);
-
-		lseek(fd_flight, 0, SEEK_SET);
-		lseek(fd_flight, (tid)*sizeof(struct flight), SEEK_CUR);
-		read(fd_flight, &tdb, sizeof(struct flight));
-		
-		read(client_sock,&choice,sizeof(int));
-		if(choice==1){							// update flight name
-			write(client_sock,&tdb.flight_name,sizeof(tdb.flight_name));
-			read(client_sock,&tdb.flight_name,sizeof(tdb.flight_name));
-			
-		}
-		else if(choice==2){						// update total number of seats
-			write(client_sock,&tdb.total_seats,sizeof(tdb.total_seats));
-			read(client_sock,&tdb.total_seats,sizeof(tdb.total_seats));
-		}
-	
-		lseek(fd_flight, -1*sizeof(struct flight), SEEK_CUR);
-		write(fd_flight, &tdb, sizeof(struct flight));
-		valid=1;
-		write(client_sock,&valid,sizeof(valid));
-		lock.l_type = F_UNLCK;
-		fcntl(fd_flight, F_SETLK, &lock);
-		close(fd_flight);	
-	}
-
-	else if(choice==4){						// Delete flight
-		crud_flight(client_sock);
-		struct flock lock;
-		struct flight tdb;
-		int fd_flight = open("db/db_flight.txt", O_RDWR);
-		int tid,valid=0;
-
-		read(client_sock,&tid,sizeof(tid));
-
-		lock.l_type = F_WRLCK;
-		lock.l_start = (tid)*sizeof(struct flight);
-		lock.l_len = sizeof(struct flight);
-		lock.l_whence = SEEK_SET;
-		lock.l_pid = getpid();
-		
-		fcntl(fd_flight, F_SETLKW, &lock);
-		
-		lseek(fd_flight, 0, SEEK_SET);
-		lseek(fd_flight, (tid)*sizeof(struct flight), SEEK_CUR);
-		read(fd_flight, &tdb, sizeof(struct flight));
-		strcpy(tdb.flight_name,"deleted");
-		lseek(fd_flight, -1*sizeof(struct flight), SEEK_CUR);
-		write(fd_flight, &tdb, sizeof(struct flight));
-		valid=1;
-		write(client_sock,&valid,sizeof(valid));
-		lock.l_type = F_UNLCK;
-		fcntl(fd_flight, F_SETLK, &lock);
-		close(fd_flight);	
-	}	
-}
-
-//---------------------- CRUD operation on user--------------------//
-void crud_user(int client_sock){
-	int valid=0;	
-	int choice;
-	read(client_sock,&choice,sizeof(choice));
-	if(choice==1){    					// Add user
-		char name[50],password[50];
-		int type;
-		read(client_sock, &type, sizeof(type));
-		read(client_sock, &name, sizeof(name));
-		read(client_sock, &password, sizeof(password));
-		
-		struct user udb;
-		struct flock lock;
-		int fd_user = open("db/db_user.txt", O_RDWR);
-		int fp = lseek(fd_user, 0, SEEK_END);
-		
-		lock.l_type = F_WRLCK;
-		lock.l_start = fp;
-		lock.l_len = 0;
-		lock.l_whence = SEEK_SET;
-		lock.l_pid = getpid();
-
-		fcntl(fd_user, F_SETLKW, &lock);
-
-		if(fp==0){
-			udb.login_id = 1;
-			strcpy(udb.name, name);
-			strcpy(udb.password, password);
-			udb.type=type;
-			write(fd_user, &udb, sizeof(udb));
-			valid = 1;
-			write(client_sock,&valid,sizeof(int));
-			write(client_sock, &udb.login_id, sizeof(udb.login_id));
-		}
-		else{
-			fp = lseek(fd_user, -1 * sizeof(struct user), SEEK_END);
-			read(fd_user, &udb, sizeof(udb));
-			udb.login_id++;
-			strcpy(udb.name, name);
-			strcpy(udb.password, password);
-			udb.type=type;
-			write(fd_user, &udb, sizeof(udb));
-			valid = 1;
-			write(client_sock,&valid,sizeof(int));
-			write(client_sock, &udb.login_id, sizeof(udb.login_id));
-		}
-		lock.l_type = F_UNLCK;
-		fcntl(fd_user, F_SETLK, &lock);
-		close(fd_user);
-		
-	}
-
-	else if(choice==2){					// View user list
-		struct flock lock;
-		struct user udb;
-		int fd_user = open("db/db_user.txt", O_RDONLY);
-		
-		lock.l_type = F_RDLCK;
-		lock.l_start = 0;
-		lock.l_len = 0;
-		lock.l_whence = SEEK_SET;
-		lock.l_pid = getpid();
-		
-		fcntl(fd_user, F_SETLKW, &lock);
-		int fp = lseek(fd_user, 0, SEEK_END);
-		int no_of_users = fp / sizeof(struct user);
-		no_of_users--;
-		write(client_sock, &no_of_users, sizeof(int));
-
-		lseek(fd_user,0,SEEK_SET);
-		while(fp != lseek(fd_user,0,SEEK_CUR)){
-			read(fd_user,&udb,sizeof(udb));
-			if(udb.type!=0){
-				write(client_sock,&udb.login_id,sizeof(int));
-				write(client_sock,&udb.name,sizeof(udb.name));
-				write(client_sock,&udb.type,sizeof(int));
-			}
-		}
-		valid = 1;
-		lock.l_type = F_UNLCK;
-		fcntl(fd_user, F_SETLK, &lock);
-		close(fd_user);
-	}
-
-	else if(choice==3){					// Update user
-		crud_user(client_sock);
-		int choice,valid=0,uid;
-		char pass[50];
-		struct flock lock;
-		struct user udb;
-		int fd_user = open("db/db_user.txt", O_RDWR);
-
-		read(client_sock,&uid,sizeof(uid));
-
-		lock.l_type = F_WRLCK;
-		lock.l_start =  (uid-1)*sizeof(struct user);
-		lock.l_len = sizeof(struct user);
-		lock.l_whence = SEEK_SET;
-		lock.l_pid = getpid();
-		
-		fcntl(fd_user, F_SETLKW, &lock);
-
-		lseek(fd_user, 0, SEEK_SET);
-		lseek(fd_user, (uid-1)*sizeof(struct user), SEEK_CUR);
-		read(fd_user, &udb, sizeof(struct user));
-		
-		read(client_sock,&choice,sizeof(int));
-		if(choice==1){					// update name
-			write(client_sock,&udb.name,sizeof(udb.name));
-			read(client_sock,&udb.name,sizeof(udb.name));
-			valid=1;
-			write(client_sock,&valid,sizeof(valid));		
-		}
-		else if(choice==2){				// update password
-			read(client_sock,&pass,sizeof(pass));
-			if(!strcmp(udb.password,pass))
-				valid = 1;
-			write(client_sock,&valid,sizeof(valid));
-			read(client_sock,&udb.password,sizeof(udb.password));
-		}
-	
-		lseek(fd_user, -1*sizeof(struct user), SEEK_CUR);
-		write(fd_user, &udb, sizeof(struct user));
-		if(valid)
-			write(client_sock,&valid,sizeof(valid));
-		lock.l_type = F_UNLCK;
-		fcntl(fd_user, F_SETLK, &lock);
-		close(fd_user);	
-	}
-
-	else if(choice==4){						// Delete any particular user
-		crud_user(client_sock);
-		struct flock lock;
-		struct user udb;
-		int fd_user = open("db/db_user.txt", O_RDWR);
-		int uid,valid=0;
-
-		read(client_sock,&uid,sizeof(uid));
-
-		lock.l_type = F_WRLCK;
-		lock.l_start =  (uid-1)*sizeof(struct user);
-		lock.l_len = sizeof(struct user);
-		lock.l_whence = SEEK_SET;
-		lock.l_pid = getpid();
-		
-		fcntl(fd_user, F_SETLKW, &lock);
-		
-		lseek(fd_user, 0, SEEK_SET);
-		lseek(fd_user, (uid-1)*sizeof(struct user), SEEK_CUR);
-		read(fd_user, &udb, sizeof(struct user));
-		strcpy(udb.name,"deleted");
-		strcpy(udb.password,"");
-		lseek(fd_user, -1*sizeof(struct user), SEEK_CUR);
-		write(fd_user, &udb, sizeof(struct user));
-		valid=1;
-		write(client_sock,&valid,sizeof(valid));
-		lock.l_type = F_UNLCK;
-		fcntl(fd_user, F_SETLK, &lock);
-		close(fd_user);	
-	}
-}
-
-
-//---------------------- User functions -----------------------//
-int user_function(int client_sock,int choice,int type,int id){
-	int valid=0;
-	if(choice==1){						// book ticket
-		crud_flight(client_sock);
-		struct flock lockt;
-		struct flock lockb;
-		struct flight tdb;
-		struct booking bdb;
-		int fd_flight = open("db/db_flight.txt", O_RDWR);
-		int fd_book = open("db/db_booking.txt", O_RDWR);
-		int tid,seats;
-		read(client_sock,&tid,sizeof(tid));		
-				
-		lockt.l_type = F_WRLCK;
-		lockt.l_start = tid*sizeof(struct flight);
-		lockt.l_len = sizeof(struct flight);
-		lockt.l_whence = SEEK_SET;
-		lockt.l_pid = getpid();
-		
-		lockb.l_type = F_WRLCK;
-		lockb.l_start = 0;
-		lockb.l_len = 0;
-		lockb.l_whence = SEEK_END;
-		lockb.l_pid = getpid();
-		
-		fcntl(fd_flight, F_SETLKW, &lockt);
-		lseek(fd_flight,tid*sizeof(struct flight),SEEK_SET);
-		
-		read(fd_flight,&tdb,sizeof(tdb));
-		read(client_sock,&seats,sizeof(seats));
-
-		if(tdb.flight_number==tid)
-		{		
-			if(tdb.available_seats>=seats){
-				valid = 1;
-				tdb.available_seats -= seats;
-				fcntl(fd_book, F_SETLKW, &lockb);
-				int fp = lseek(fd_book, 0, SEEK_END);
-				
-				if(fp > 0){
-					lseek(fd_book, -1*sizeof(struct booking), SEEK_CUR);
-					read(fd_book, &bdb, sizeof(struct booking));
-					bdb.booking_id++;
-				}
-				else 
-					bdb.booking_id = 0;
-
-				bdb.type = type;
-				bdb.uid = id;
-				bdb.tid = tid;
-				bdb.seats = seats;
-				write(fd_book, &bdb, sizeof(struct booking));
-				lockb.l_type = F_UNLCK;
-				fcntl(fd_book, F_SETLK, &lockb);
-			 	close(fd_book);
-			}
-		
-		lseek(fd_flight, -1*sizeof(struct flight), SEEK_CUR);
-		write(fd_flight, &tdb, sizeof(tdb));
-		}
-
-		lockt.l_type = F_UNLCK;
-		fcntl(fd_flight, F_SETLK, &lockt);
-		close(fd_flight);
-		write(client_sock,&valid,sizeof(valid));
-		return valid;		
-	}
-	
-	else if(choice==2){							// View bookings
-		struct flock lock;
-		struct booking bdb;
-		int fd_book = open("db/db_booking.txt", O_RDONLY);
-		int no_of_bookings = 0;
-	
-		lock.l_type = F_RDLCK;
-		lock.l_start = 0;
-		lock.l_len = 0;
-		lock.l_whence = SEEK_SET;
-		lock.l_pid = getpid();
-		
-		fcntl(fd_book, F_SETLKW, &lock);
-	
-		while(read(fd_book,&bdb,sizeof(bdb))){
-			if (bdb.uid==id)
-				no_of_bookings++;
-		}
-
-		write(client_sock, &no_of_bookings, sizeof(int));
-		lseek(fd_book,0,SEEK_SET);
-
-		while(read(fd_book,&bdb,sizeof(bdb))){
-			if(bdb.uid==id){
-				write(client_sock,&bdb.booking_id,sizeof(int));
-				write(client_sock,&bdb.tid,sizeof(int));
-				write(client_sock,&bdb.seats,sizeof(int));
-			}
-		}
-		lock.l_type = F_UNLCK;
-		fcntl(fd_book, F_SETLK, &lock);
-		close(fd_book);
-		return valid;
-	}
-
-	else if (choice==3){							// update booking
-		int choice = 2,bid,val;
-		user_function(client_sock,choice,type,id);
-		struct booking bdb;
-		struct flight tdb;
-		struct flock lockb;
-		struct flock lockt;
-		int fd_book = open("db/db_booking.txt", O_RDWR);
-		int fd_flight = open("db/db_flight.txt", O_RDWR);
-		read(client_sock,&bid,sizeof(bid));
-
-		lockb.l_type = F_WRLCK;
-		lockb.l_start = bid*sizeof(struct booking);
-		lockb.l_len = sizeof(struct booking);
-		lockb.l_whence = SEEK_SET;
-		lockb.l_pid = getpid();
-		
-		fcntl(fd_book, F_SETLKW, &lockb);
-		lseek(fd_book,bid*sizeof(struct booking),SEEK_SET);
-		read(fd_book,&bdb,sizeof(bdb));
-		lseek(fd_book,-1*sizeof(struct booking),SEEK_CUR);
-		
-		lockt.l_type = F_WRLCK;
-		lockt.l_start = (bdb.tid)*sizeof(struct flight);
-		lockt.l_len = sizeof(struct flight);
-		lockt.l_whence = SEEK_SET;
-		lockt.l_pid = getpid();
-
-		fcntl(fd_flight, F_SETLKW, &lockt);
-		lseek(fd_flight,(bdb.tid)*sizeof(struct flight),SEEK_SET);
-		read(fd_flight,&tdb,sizeof(tdb));
-		lseek(fd_flight,-1*sizeof(struct flight),SEEK_CUR);
-
-		read(client_sock,&choice,sizeof(choice));
-	
-		if(choice==1){							// increase number of seats required of booking id
-			read(client_sock,&val,sizeof(val));
-			if(tdb.available_seats>=val){
-				valid=1;
-				tdb.available_seats -= val;
-				bdb.seats += val;
-			}
-		}
-		else if(choice==2){						// decrease number of seats required of booking id
-			valid=1;
-			read(client_sock,&val,sizeof(val));
-			tdb.available_seats += val;
-			bdb.seats -= val;	
-		}
-		
-		write(fd_flight,&tdb,sizeof(tdb));
-		lockt.l_type = F_UNLCK;
-		fcntl(fd_flight, F_SETLK, &lockt);
-		close(fd_flight);
-		
-		write(fd_book,&bdb,sizeof(bdb));
-		lockb.l_type = F_UNLCK;
-		fcntl(fd_book, F_SETLK, &lockb);
-		close(fd_book);
-		
-		write(client_sock,&valid,sizeof(valid));
-		return valid;
-	}
-	else if(choice==4){							// Cancel an entire booking
-		int choice = 2,bid;
-		user_function(client_sock,choice,type,id);
-		struct booking bdb;
-		struct flight tdb;
-		struct flock lockb;
-		struct flock lockt;
-		int fd_book = open("db/db_booking.txt", O_RDWR);
-		int fd_flight = open("db/db_flight.txt", O_RDWR);
-		read(client_sock,&bid,sizeof(bid));
-
-		lockb.l_type = F_WRLCK;
-		lockb.l_start = bid*sizeof(struct booking);
-		lockb.l_len = sizeof(struct booking);
-		lockb.l_whence = SEEK_SET;
-		lockb.l_pid = getpid();
-		
-		fcntl(fd_book, F_SETLKW, &lockb);
-		lseek(fd_book,bid*sizeof(struct booking),SEEK_SET);
-		read(fd_book,&bdb,sizeof(bdb));
-		lseek(fd_book,-1*sizeof(struct booking),SEEK_CUR);
-		
-		lockt.l_type = F_WRLCK;
-		lockt.l_start = (bdb.tid)*sizeof(struct flight);
-		lockt.l_len = sizeof(struct flight);
-		lockt.l_whence = SEEK_SET;
-		lockt.l_pid = getpid();
-
-		fcntl(fd_flight, F_SETLKW, &lockt);
-		lseek(fd_flight,(bdb.tid)*sizeof(struct flight),SEEK_SET);
-		read(fd_flight,&tdb,sizeof(tdb));
-		lseek(fd_flight,-1*sizeof(struct flight),SEEK_CUR);
-
-		tdb.available_seats += bdb.seats;
-		bdb.seats = 0;
-		valid = 1;
-
-		write(fd_flight,&tdb,sizeof(tdb));
-		lockt.l_type = F_UNLCK;
-		fcntl(fd_flight, F_SETLK, &lockt);
-		close(fd_flight);
-		
-		write(fd_book,&bdb,sizeof(bdb));
-		lockb.l_type = F_UNLCK;
-		fcntl(fd_book, F_SETLK, &lockb);
-		close(fd_book);
-		
-		write(client_sock,&valid,sizeof(valid));
-		return valid;
-		
-	}
-	else if(choice==5)										// Logout
-		return 5;
-
 }
